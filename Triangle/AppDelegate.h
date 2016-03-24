2fdd23b10fd9b683e6b2cdee9b9b3896b567989b
Doug Voss <douglas.c.voss@gmail.com>
2016-03-23 22:51:42 -0600

    Initial commit

---
+++ Triangle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Triangle
+//
+//  Created by Douglas Voss on 3/23/16.
+//  Copyright © 2016 VossWareLLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Triangle/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Triangle
+//
+//  Created by Douglas Voss on 3/23/16.
+//  Copyright © 2016 VossWareLLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
